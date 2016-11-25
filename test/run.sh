8859eeb5aac941c1ecbc12bd332fbb75116f2cf6
Tim Snowhite <tim@snowhitesolutions.com>
2016-11-25 18:54:08 -0500

    [travis] don’t exit after each test

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eux -o pipefail
-for i in examples/*; do ./bert $i 1> ./test/$i.stdout 2> ./test/$i.stderr; done
+for i in examples/*; do ./bert $i 1> ./test/$i.stdout 2> ./test/$i.stderr || true; done
 git diff
