3df612de9c3cff2ea3ba8e5f887c76a8126ce77b
Tim Snowhite <tim@snowhitesolutions.com>
2016-11-25 19:05:16 -0500

    [travis] just use one file

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -eux -o pipefail
+cd src/
+make || true
+mv bert ../
+cd ..
 for i in examples/*; do ./bert $i 1> ./test/$i.stdout 2> ./test/$i.stderr || true; done
 git add test
 git diff --cached -w
